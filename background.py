dbfabe30a4effe4fe57b3e6b069b9c4686266810
sanchik7777 <80630095+sanchik7777@users.noreply.github.com>
2024-12-13 11:56:56 +0200

    Update background.py

--- background.py
+++ background.py
@@ -3,13 +3,14 @@ from flask import request
 from threading import Thread
 import time
 import requests
-
+import logging
 
 app = Flask('')
 
 @app.route('/')
 def home():
-  return "bot online"
+    logging.info(f"Запрос получен: {request.method} от {request.remote_addr}")
+    return "bot online"
 
 def run():
   app.run(host='0.0.0.0', port=8000)
